3783825df6af207ffd22d1a83e975eb87605de0a
pashangshangpo <pashangshangpo@gmail.com>
2020-07-12 13:43:00 +0800

    feat: 添加支持命令行工具

---
+++ bin/demo.js
@@ -0,0 +1,22 @@
+const Path = require('path')
+const static = require('koa-static')
+const Koa = require('koa')
+const app = new Koa()
+
+module.exports = indexPage => {
+  app.use(async (cxt, next) => {
+    if (cxt.url === '/') {
+      cxt.body = indexPage
+      
+      return
+    }
+
+    await next()
+  })
+
+  app.use(static(Path.join(__dirname, '../dist')))
+
+  app.listen(7900, () => {
+    console.log('http://127.0.0.1:7900/#/')
+  })
+}
---
+++ bin/index.js
@@ -0,0 +1,53 @@
+#!/usr/bin/env node
+
+Promise.resolve().then(async () => {
+  const Cli = require('commander')
+  const Compressing = require('compressing')
+  const CopyDir = require('copy-dir')
+  const Fs = require('fs')
+  const Path = require('path')
+  const Demo = require('./demo')
+
+  Cli
+    .version('0.0.1')
+    .description('i-ppt')
+    .option('-t, --type [type]', '执行的命令类型，示例：demo,export')
+    .option('-path, --path [type]', '文件路径，绝对路径')
+    .parse(process.argv)
+
+  const Type = Cli.type || 'demo'
+  const FilePath = Cli.path
+
+  if (Type === 'export' && !FilePath) {
+    console.log('必须指定md文件路径')
+    process.exit()
+  }
+
+  if (FilePath) {
+    const md = Fs.readFileSync(FilePath).toString()
+    const distPath = Path.join(__dirname, '../dist')
+    const htmlPath = Path.join(distPath, 'index.html')
+    let html = Fs.readFileSync(htmlPath).toString()
+
+    html = html.replace('window.MD', `window.MD = \`${md}\``)
+  
+    if (Type === 'demo') {
+      Demo(html)
+      return
+    }
+
+    const demoOutPath = Path.join(distPath, '../i-ppt-demo')
+
+    CopyDir.sync(distPath, demoOutPath)
+
+    Fs.writeFileSync(Path.join(demoOutPath, 'index.html'), html)
+
+    Compressing.zip.compressDir(demoOutPath, Path.resolve('.', 'i-ppt-demo.zip'))
+
+    return
+  }
+
+  if (Type === 'demo') {
+    Demo()
+  }
+})
